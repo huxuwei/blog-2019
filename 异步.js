0d0372f874eb512b7c9a9f3815e6b2fb076f72b3
huxuwei <fanghuiling@boluozaixian.com>
2019-10-10 18:41:27 +0800

    异步

--- 异步.js
+++ 异步.js
@@ -28,21 +28,57 @@
 
 // async/await
 {
-  async function async1() {
-    console.log('async1 start')
-    await async2()
-    console.log('async1 end')
-  }
+  // async function async1() {
+  //   console.log('async1 start')
+  //   await async2()
+  //   console.log('async1 end')
+  // }
 
-  async function async2() {
-    console.log('async')
-  }
-  console.log('start')
-  async1()
-  console.log('end')
-  
+  // async function async2() {
+  //   console.log('async')
+  // }
+  // console.log('start')
+  // async1()
+  // console.log('end')
+
 }
 
 
 
 
+{
+  console.log('script start');
+
+  setTimeout(function() {
+    console.log('setTimeout');
+  }, 0);
+
+  Promise.resolve().then(function() {
+    console.log('promise1');
+  }).then(function() {
+    console.log('promise2');
+  });
+  console.log('script end');
+
+// 作者：光光同学
+// 链接：https://juejin.im/post/5c3d8956e51d4511dc72c200
+// 来源：掘金
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
+
+{
+  function Promise(???){
+    ???
+    return ???
+  }
+
+  var promise = new Promise(function(x,y){
+      setTimeout(()=>{
+          x(101)
+      }, 3000)
+  })
+  promise.then((z)=>{
+      console.log(z)  // 101
+  })
+
+}
